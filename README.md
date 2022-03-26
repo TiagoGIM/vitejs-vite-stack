7e0ec0c65025b3571b7caf8ddd4b5d11699471b8
TiagoGIM <herique.sa@gmail.com>
2022-03-26 14:10:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-tufnsf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-tufnsf)
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h2>HOMEssssssssssssssssssssssssssssssss</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {
+    Counter,
+  },
+};
+</script>
